7bdf548c175caec05e3294f84dce3189e6badadd
Benjamin DENEUX <b.deneux@okp4.com>
2022-10-18 23:03:50 +0200

    feat(docs): change struct comment to be displayed on generated docs

--- contracts/cw-template/src/msg.rs
+++ contracts/cw-template/src/msg.rs
@@ -1,25 +1,28 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 
+/// Instantiate messages
 #[cw_serde]
 pub struct InstantiateMsg {
     pub count: i32,
 }
 
+/// Execute messages
 #[cw_serde]
 pub enum ExecuteMsg {
     Increment {},
     Reset { count: i32 },
 }
 
+/// Query messages
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
+    /// GetCount returns the current count as a json-encoded number
     #[returns(GetCountResponse)]
     GetCount {},
 }
 
-// We define a custom struct for each query response
+/// We define a custom struct for each query response
 #[cw_serde]
 pub struct GetCountResponse {
     pub count: i32,

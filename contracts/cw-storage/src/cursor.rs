79990be5376b454efd6797313743869e1a312903
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2023-03-10 15:36:58 +0100

    test(storage): add cursor encoding tests

--- contracts/cw-storage/src/cursor.rs
+++ contracts/cw-storage/src/cursor.rs
@@ -12,3 +12,38 @@ pub fn decode(cursor: String) -> StdResult<Cursor> {
 
     String::from_utf8(raw).map_err(|err| StdError::parse_err("Cursor", err))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn proper_encode() {
+        assert_eq!(encode("".to_string()), "".to_string());
+        assert_eq!(encode("an_id".to_string()), "BzZCCcK".to_string());
+    }
+
+    #[test]
+    fn proper_decode() {
+        assert_eq!(decode("".to_string()), Ok("".to_string()));
+        assert_eq!(decode("BzZCCcK".to_string()), Ok("an_id".to_string()));
+    }
+
+    #[test]
+    fn invalid_decode() {
+        assert_eq!(
+            decode("?".to_string()),
+            Err(StdError::parse_err(
+                "Cursor",
+                "provided string contained invalid character '?' at byte 0"
+            ))
+        );
+        assert_eq!(
+            decode("VtB5VXc".to_string()),
+            Err(StdError::parse_err(
+                "Cursor",
+                "invalid utf-8 sequence of 1 bytes from index 0"
+            ))
+        );
+    }
+}

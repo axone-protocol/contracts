ed12830af1732beddcbc881cfddfaa174a2bb46c
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2023-03-30 16:35:41 +0200

    test(law-stone): cover logic ask response parsing

--- contracts/cw-law-stone/src/helper.rs
+++ contracts/cw-law-stone/src/helper.rs
@@ -67,3 +67,61 @@ pub fn ask_response_to_objects(
         })
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use logic_bindings::error::TermParseError;
+    use logic_bindings::{Answer, Term};
+
+    #[test]
+    fn logic_to_objects() {
+        let cases = vec![
+            ("[]".to_string(), Ok(vec![])),
+            ("['cosmwasm:cw-storage:okp41ffzp0xmjhwkltuxcvccl0z9tyfuu7txp5ke0tpkcjpzuq9fcj3pqrteqt3?query=%7B%22object_data%22%3A%7B%22id%22%3A%224cbe36399aabfcc7158ee7a66cbfffa525bb0ceab33d1ff2cff08759fe0a9b05%22%7D%7D']".to_string(), Ok(vec![ObjectRef{
+                object_id: "4cbe36399aabfcc7158ee7a66cbfffa525bb0ceab33d1ff2cff08759fe0a9b05".to_string(),
+                storage_address: "okp41ffzp0xmjhwkltuxcvccl0z9tyfuu7txp5ke0tpkcjpzuq9fcj3pqrteqt3".to_string(),
+            }])),
+            ("['cosmwasm:cw-storage:okp41ffzp0xmjhwkltuxcvccl0z9tyfuu7txp5ke0tpkcjpzuq9fcj3pqrteqt3?query=%7B%22object_data%22%3A%7B%22id%22%3A%224cbe36399aabfcc7158ee7a66cbfffa525bb0ceab33d1ff2cff08759fe0a9b05%22%7D%7D','cosmwasm:cw-storage:okp41cxmx7su8h5pvqca85cxdylz86uj9x9gu5xuqv34kw87q5x0hexds8w44jg?query=%7B%22object_data%22%3A%7B%22id%22%3A%221485133dd3ab4b1c4b8085e7265585f91ae3cca0996a39e0377a1059296f6aa7%22%7D%7D']".to_string(), Ok(vec![ObjectRef{
+                object_id: "4cbe36399aabfcc7158ee7a66cbfffa525bb0ceab33d1ff2cff08759fe0a9b05".to_string(),
+                storage_address: "okp41ffzp0xmjhwkltuxcvccl0z9tyfuu7txp5ke0tpkcjpzuq9fcj3pqrteqt3".to_string(),
+            },ObjectRef{
+                object_id: "1485133dd3ab4b1c4b8085e7265585f91ae3cca0996a39e0377a1059296f6aa7".to_string(),
+                storage_address: "okp41cxmx7su8h5pvqca85cxdylz86uj9x9gu5xuqv34kw87q5x0hexds8w44jg".to_string(),
+            }])),
+            ("[,]".to_string(), Err(ContractError::LogicAskResponse(LogicAskResponseError::Parse(TermParseError::EmptyValue)))),
+            ("(1,2)".to_string(), Err(ContractError::LogicAskResponse(LogicAskResponseError::UnexpectedTerm))),
+            ("[[]]".to_string(), Err(ContractError::LogicAskResponse(LogicAskResponseError::UnexpectedTerm))),
+            ("[[]]".to_string(), Err(ContractError::LogicAskResponse(LogicAskResponseError::UnexpectedTerm))),
+            ("['nawak']".to_string(), Err(ContractError::ParseCosmwasmUri(CosmwasmUriError::ParseURI(url::ParseError::RelativeUrlWithoutBase)))),
+            ("['cosmwasm:addr?query=%7B%22object%22%3A%7B%22id%22%3A%221485133dd3ab4b1c4b8085e7265585f91ae3cca0996a39e0377a1059296f6aa7%22%7D%7D']".to_string(), Err(ContractError::ParseCosmwasmUri(CosmwasmUriError::Malformed("wrong query content".to_string())))),
+        ];
+
+        for case in cases {
+            assert_eq!(
+                ask_response_to_objects(
+                    AskResponse {
+                        answer: Some(Answer {
+                            results: vec![logic_bindings::Result {
+                                substitutions: vec![Substitution {
+                                    variable: "X".to_string(),
+                                    term: Term {
+                                        name: case.0,
+                                        arguments: vec![],
+                                    }
+                                }]
+                            }],
+                            has_more: false,
+                            success: true,
+                            variables: vec![],
+                        }),
+                        height: 1,
+                        gas_used: 1,
+                    },
+                    "X".to_string()
+                ),
+                case.1
+            );
+        }
+    }
+}

1edacf6748b2b1fd0a17e801e1336fd608f115ef
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2023-04-11 22:41:21 +0200

    feat(cognitarium): design instantiate message

--- contracts/okp4-cognitarium/src/msg.rs
+++ contracts/okp4-cognitarium/src/msg.rs
@@ -2,7 +2,10 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 
 /// Instantiate message
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    /// Limitations regarding store usage.
+    pub limits: StoreLimits,
+}
 
 /// Execute messages
 #[cw_serde]
@@ -12,3 +15,12 @@ pub enum ExecuteMsg {}
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {}
+
+/// # StoreLimits
+/// Contains limitations regarding store usages.
+#[cw_serde]
+pub struct StoreLimits {
+    /// max_triple_count denotes the maximum number of triples the store can contains.
+    /// If None, there is no limit on the number of triples.
+    pub max_triple_count: Option<Uint128>,
+}

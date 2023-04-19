1f8c0d085433648172e94056b4ab167ac1a722a1
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2023-04-19 13:59:15 +0200

    feat(cognitarium): implement rdf parsing

--- contracts/okp4-cognitarium/src/rdf.rs
+++ contracts/okp4-cognitarium/src/rdf.rs
@@ -1,4 +1,29 @@
+use crate::msg::DataInput;
 use cosmwasm_std::StdError;
+use rio_api::model::Triple;
+use rio_api::parser::TriplesParser;
+use rio_turtle::{NTriplesParser, TurtleError, TurtleParser};
+use rio_xml::{RdfXmlError, RdfXmlParser};
+use std::io::BufReader;
+
+pub fn parse_triples<E, T, MF, UF>(input: DataInput, map_fn: MF, use_fn: UF) -> Result<(), E>
+where
+    MF: FnMut(Triple<'_>) -> Result<T, E>,
+    UF: FnMut(Result<T, E>) -> Result<(), E>,
+    E: From<RdfXmlError> + From<TurtleError>,
+{
+    match input {
+        DataInput::RDFXml(data) => RdfXmlParser::new(BufReader::new(data.as_slice()), None)
+            .into_iter(map_fn)
+            .try_for_each(use_fn),
+        DataInput::Turtle(data) => TurtleParser::new(BufReader::new(data.as_slice()), None)
+            .into_iter(map_fn)
+            .try_for_each(use_fn),
+        DataInput::NTriples(data) => NTriplesParser::new(BufReader::new(data.as_slice()))
+            .into_iter(map_fn)
+            .try_for_each(use_fn),
+    }
+}
 
 pub fn explode_iri(iri: &str) -> Result<(&str, &str), StdError> {
     for delim in ['#', '/'] {

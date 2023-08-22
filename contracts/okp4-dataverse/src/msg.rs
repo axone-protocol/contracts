8faf0c1d2946c0f9640fbf5312f426556414548d
ccamel <camel.christophe@gmail.com>
2023-08-22 15:37:44 +0200

    feat(dataverse): specify RegisterService execute message

--- contracts/okp4-dataverse/src/msg.rs
+++ contracts/okp4-dataverse/src/msg.rs
@@ -9,9 +9,34 @@ pub struct InstantiateMsg {
     pub name: String,
 }
 
+/// DID is a type alias for a string that represents a Decentralized Identifier (DID).
+type DID = String;
+
+/// URI is a type alias for a string that represents a URI.
+type URI = String;
+
 /// Execute messages
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    /// # RegisterService
+    /// RegisterService registers a service in the dataverse.
+    /// Service is a generic concept for any kind of service that can be provided through a network (e.g. a REST API, a gRPC service, etc.).
+    ///
+    /// Each service is identified and located by its unique [URI](https://en.wikipedia.org/wiki/Uniform_Resource_Identifier) which defines the entry point
+    /// of the service.
+    RegisterService {
+        /// Identity of the service.
+        identity: DID,
+
+        /// Identifier of the service.
+        /// This identifier is unique within the scope of the dataverse and is used to reference the service and describe how to interact with it.
+        identifier: URI,
+
+        /// References the individual, company, or organization that is responsible for registering and
+        /// managing the service.
+        registrar: URI,
+    },
+}
 
 /// Query messages
 #[cw_serde]

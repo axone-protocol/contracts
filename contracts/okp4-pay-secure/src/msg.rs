41ff9744f2f25b29364c5758516505e4d8155579
ccamel <camel.christophe@gmail.com>
2023-08-31 13:17:33 +0200

    feat(pay-secure): specify contract instantiation

--- contracts/okp4-pay-secure/src/msg.rs
+++ contracts/okp4-pay-secure/src/msg.rs
@@ -1,4 +1,19 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Coin;
+use cw_utils::Expiration;
+
+/// Message to initialize a new instance of the contract, which represents a new Account,
+/// owned by the Holder, which is the sender of the message.
+#[cw_serde]
+pub struct InstantiateMsg {
+    /// The name of the account. It can be used to provide a human-readable name for the account.
+    /// This is an optional field. If not provided, the account will be left unnamed.
+    pub name: Option<String>,
+    /// The list of denominations that are accepted by the account.
+    /// If not provided, the account will accept all denominations.
+    pub accepted_denoms: Option<Vec<String>>,
+}
+
 
 /// Instantiate message
 #[cw_serde]

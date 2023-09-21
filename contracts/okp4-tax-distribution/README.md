28d0a04566619676e16d826f31c4d8f9768f9b51
Benjamin DENEUX <b.deneux@okp4.com>
2023-09-21 15:25:04 +0200

    feat(distribution): first distribution contract schema

--- contracts/okp4-tax-distribution/README.md
+++ contracts/okp4-tax-distribution/README.md
@@ -1,1 +1,21 @@
-# okp4-tax-distribution
+# Tax distribution
+
+## Overview
+
+This `okp4-tax-distribution` smart contract is designed to facilitate automated distribution of taxes or royalties to 
+various recipients on a blockchain. It enables developers to configure tax recipients, including burn and community pool, 
+their distribution ratios, and efficiently manage funds. 
+
+The contract is intended to be used by other contracts that require automated tax or royalty distribution. This contract 
+is also intended to be instantiated and managed by governance (through the CosmWasm governance proposal), so that each 
+parameter configuration and update can be put to a vote. 
+
+## Usage
+
+### Instantiate
+
+
+### Execute
+
+
+### Query
--- contracts/okp4-tax-distribution/src/msg.rs
+++ contracts/okp4-tax-distribution/src/msg.rs
@@ -1,28 +1,80 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Decimal;
 
-/// Instantiate message
+/// InstantiateMsg is the default config used for the tax distribution contract.
+///
+/// This contract is intended to be stored with `--instantiate-nobody ` args to prevent only the
+/// governance that is allowed to configure and instantiate this contract and be set as the contract
+/// admin.
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    /// Configure the destination of the remaining tokens after distribution to other recipients
+    /// (defined in `destinations`).
+    default_recipient: Recipient,
+    /// Define the distribution rate of tokens to the intended recipients.
+    /// The total rate sum should not exceed 1.
+    destinations: Vec<Destination>,
+}
+
+#[cw_serde]
+pub enum Recipient {
+    /// Send token to the community pool.
+    CommunityPool,
+    /// Burn token.
+    Burn,
+    /// Send token to a specific wallet address.
+    Address(String),
+}
+
+#[cw_serde]
+pub struct Destination {
+    /// Recipient of token
+    recipient: Recipient,
+    /// Set the token rate to receive for this recipient.
+    /// Value should be between zero exclusive and one exclusive.
+    ratio: Decimal,
+}
 
 /// Execute messages
 #[cw_serde]
 pub enum ExecuteMsg {
-    /// # Foo
-    Foo,
+    /// # Distribute
+    /// Distributes the tokens received from the transaction to the recipients following
+    /// the configured apportionment.
+    Distribute,
+
+    /// # UpdateDefaultRecipient
+    /// Change the default recipient used for distribute remaining token.
+    ///
+    /// Only contract admin can update the default recipient.
+    UpdateDefaultRecipient { recipient: Recipient },
+
+    /// # AddDestination
+    /// Add a new recipient for receiving token with it's corresponding ratio.
+    /// Don't forget that the total ratio already configured shouldn't exceed 1.
+    AddDestination { destination: Destination },
+
+    /// # RemoveRecipient
+    /// Remove a recipient from the tax distribution.
+    RemoveRecipient { recipient: Recipient },
 }
 
 /// Query messages
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    /// # Bar
-    #[returns(BarResponse)]
-    Bar { foo: String },
+    /// # Destinations
+    /// Returns the current configuration for all tax distribution destinations with their
+    /// corresponding ratio.
+    #[returns(DestinationsResponse)]
+    Destinations,
 }
 
-/// # BarResponse
+/// # DestinationsResponse
 #[cw_serde]
-pub struct BarResponse {
-    /// The foo value
-    pub foo: String,
+pub struct DestinationsResponse {
+    /// The current configured default recipient for remaining token after distribution.
+    pub default_recipient: Recipient,
+    /// All recipients with their corresponding token ratio.
+    pub destinations: Vec<Destination>,
 }

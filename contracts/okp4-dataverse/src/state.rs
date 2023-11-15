1c05850dd8c1f87b8680ce19c7e8c24b1a497999
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2023-11-15 16:40:07 +0100

    feat(dataverse): add basic internal state

--- contracts/okp4-dataverse/src/state.rs
+++ contracts/okp4-dataverse/src/state.rs
@@ -1,1 +1,10 @@
+use cw_storage_plus::Item;
+use serde::{Deserialize, Serialize};
 
+pub const DATAVERSE: Item<'_, Dataverse> = Item::new("dataverse");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct Dataverse {
+    pub name: String,
+    pub triplestore_address: String,
+}

2914238008e085db214ea11939d3cf3c1d05fb2c
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-02-16 16:38:38 +0100

    test(dataverse): add credential mapping tests

--- contracts/okp4-dataverse/src/registrar/credential.rs
+++ contracts/okp4-dataverse/src/registrar/credential.rs
@@ -96,3 +96,85 @@ impl<'a> TryFrom<(Addr, &'a VerifiableCredential<'a>)> for DataverseCredential<'
         })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::testutil::testutil;
+    use okp4_rdf::dataset::Dataset;
+    use rio_api::model::Literal;
+
+    #[test]
+    fn proper_from_verifiable() {
+        let owned_quads = testutil::read_test_quads("vc-valid.nq");
+        let dataset = Dataset::from(owned_quads.as_slice());
+        let vc = VerifiableCredential::try_from(&dataset).unwrap();
+        let dc_res = DataverseCredential::try_from((
+            Addr::unchecked("okp41072nc6egexqr2v6vpp7yxwm68plvqnkf6xsytf"),
+            &vc,
+        ));
+
+        assert!(dc_res.is_ok());
+        assert_eq!(dc_res.unwrap(), DataverseCredential {
+                submitter_addr: Addr::unchecked("okp41072nc6egexqr2v6vpp7yxwm68plvqnkf6xsytf"),
+                id: "https://w3id.org/okp4/ontology/vnext/schema/credential/digital-service/description/72cab400-5bd6-4eb4-8605-a5ee8c1a45c9",
+                issuer: "did:key:zQ3shs7auhJSmVJpiUbQWco6bxxEhSqWnVEPvaBHBRvBKw6Q3",
+                r#type: "https://w3id.org/okp4/ontology/vnext/schema/credential/digital-service/description/DigitalServiceDescriptionCredential",
+                valid_from: "2024-01-22T00:00:00",
+                valid_until: Some("2025-01-22T00:00:00"),
+                subject: "did:key:zQ3shhb4SvzBRLbBonsvKb3WX6WoDeKWHpsXXXMhAJETqXAfB",
+                claim: vec![Triple {
+                    subject: BlankNode {id: "c0"}.into(),
+                    predicate: NamedNode {iri: "https://w3id.org/okp4/ontology/vnext/schema/credential/digital-service/description/hasCategory"},
+                    object: NamedNode {iri: "https://w3id.org/okp4/ontology/vnext/thesaurus/digital-service-category/Storage"}.into(),
+                },Triple {
+                    subject: BlankNode {id: "c0"}.into(),
+                    predicate: NamedNode {iri: "https://w3id.org/okp4/ontology/vnext/schema/credential/digital-service/description/hasTag"},
+                    object: Literal::Simple {value: "Cloud"}.into(),
+                },Triple {
+                    subject: NamedNode {iri: "https://w3id.org/okp4/ontology/vnext/schema/credential/digital-service/description/72cab400-5bd6-4eb4-8605-a5ee8c1a45c9"}.into(),
+                    predicate: NamedNode {iri: "dataverse:credential#claim"},
+                    object: BlankNode {id: "c0"}.into(),
+                }],
+            })
+    }
+
+    #[test]
+    fn unsupported_from_verifiable() {
+        let cases = vec![
+            (
+                "vc-unsupported-1.nq",
+                "credential is expected to have exactly one type",
+            ),
+            (
+                "vc-unsupported-2.nq",
+                "credential is expected to have exactly one type",
+            ),
+            (
+                "vc-unsupported-3.nq",
+                "credential is expected to contain exactly one claim",
+            ),
+            (
+                "vc-unsupported-4.nq",
+                "claim hierarchy can be forge only through blank nodes",
+            ),
+        ];
+
+        for case in cases {
+            let owned_quads = testutil::read_test_quads(case.0);
+            let dataset = Dataset::from(owned_quads.as_slice());
+            let vc = VerifiableCredential::try_from(&dataset).unwrap();
+            let dc_res = DataverseCredential::try_from((
+                Addr::unchecked("okp41072nc6egexqr2v6vpp7yxwm68plvqnkf6xsytf"),
+                &vc,
+            ));
+
+            assert!(dc_res.is_err());
+            if let ContractError::UnsupportedCredential(msg) = dc_res.err().unwrap() {
+                assert_eq!(msg, case.1.to_string());
+            } else {
+                assert!(false);
+            }
+        }
+    }
+}

df85612410134b126406e979f95487487cc443a5
Benjamin DENEUX <benjamin.deneux@proton.me>
2024-05-06 10:20:06 +0200

    refactor(renaming): change okp4-law-stone to axone-law-stone

--- contracts/axone-law-stone/src/bin/schema.rs
+++ contracts/axone-law-stone/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use okp4_law_stone::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use axone_law_stone::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {

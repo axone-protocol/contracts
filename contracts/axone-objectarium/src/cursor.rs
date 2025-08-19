47cbf6a70619c2eb2de50c2508b59c81dda54631
ccamel <camel.christophe@gmail.com>
2025-08-19 20:49:20 +0200

    refactor(cursor)!: migrate cursor encoding to bin-it for structured serialization
    This introduces a breaking change: previously generated cursors are no longer valid,
    as we now use bin-it for consistent binary encoding instead of ad-hoc base58 hashing.

--- contracts/axone-objectarium/src/cursor.rs
+++ contracts/axone-objectarium/src/cursor.rs
@@ -1,19 +1,8 @@
 use crate::crypto::Hash;
 use crate::msg::Cursor;
-use crate::state::{Object, Pin};
+use crate::state::{Object, Pin, PinPK};
+use bin_it::{BinaryReader, BinaryWriter};
 use cosmwasm_std::{Addr, StdError, StdResult};
-
-pub fn encode<I: AsRef<[u8]>>(id: I) -> Cursor {
-    bs58::encode(id).into_string()
-}
-
-pub fn decode<I: AsRef<[u8]>>(cursor: I) -> StdResult<Cursor> {
-    let raw = bs58::decode(cursor)
-        .into_vec()
-        .map_err(|err| StdError::parse_err("Cursor", err))?;
-
-    String::from_utf8(raw).map_err(|err| StdError::parse_err("Cursor", err))
-}
 
 pub trait AsCursor<PK> {
     fn encode_cursor(&self) -> StdResult<Cursor>;
@@ -22,90 +11,51 @@ pub trait AsCursor<PK> {
 
 impl AsCursor<Hash> for Object {
     fn encode_cursor(&self) -> StdResult<Cursor> {
-        Ok(bs58::encode(&self.id).into_string())
+        let mut writer = BinaryWriter::new();
+        writer.write_vec_u8(self.id.as_ref());
+
+        Ok(bs58::encode(writer.get_data()).into_string())
     }
 
     fn decode_cursor(cursor: Cursor) -> StdResult<Hash> {
-        bs58::decode(cursor)
+        let decoded = bs58::decode(cursor)
             .into_vec()
-            .map(Into::into)
-            .map_err(|err| StdError::parse_err("Cursor", err))
+            .map_err(|err| StdError::parse_err("Cursor", err))?;
+
+        let mut reader = BinaryReader::new(&decoded);
+        let hash = reader
+            .read_vec_u8()
+            .map_err(|err| StdError::parse_err("Cursor", err))?;
+
+        Ok(hash.into())
     }
 }
 
-impl AsCursor<(Addr, Hash)> for Pin {
+impl AsCursor<PinPK> for Pin {
     fn encode_cursor(&self) -> StdResult<Cursor> {
-        let pair = (self.address.as_str(), &self.id);
-        let json =
-            serde_json::to_vec(&pair).map_err(|err| StdError::serialize_err("Cursor", err))?;
+        let mut writer = BinaryWriter::new();
+        writer.write_vec_u8(self.id.as_ref());
+        writer.write_string(self.address.as_str());
 
-        Ok(bs58::encode(json).into_string())
-    }
-
-    fn decode_cursor(cursor: Cursor) -> StdResult<(Addr, Hash)> {
-        let data = bs58::decode(&cursor)
-            .into_vec()
-            .map_err(|err| StdError::parse_err("Cursor", err.to_string()))?;
-
-        let (addr_str, hash_bytes): (String, Vec<u8>) = serde_json::from_slice(&data)
-            .map_err(|err| StdError::parse_err("Cursor", err.to_string()))?;
-
-        Ok((Addr::unchecked(addr_str), hash_bytes.into()))
-    }
-}
-
-impl AsCursor<(Hash, Addr)> for Pin {
-    fn encode_cursor(&self) -> StdResult<Cursor> {
-        let pair = (&self.id, self.address.as_str());
-        let json =
-            serde_json::to_vec(&pair).map_err(|err| StdError::serialize_err("Cursor", err))?;
-
-        Ok(bs58::encode(json).into_string())
+        Ok(bs58::encode(writer.get_data()).into_string())
     }
 
     fn decode_cursor(cursor: Cursor) -> StdResult<(Hash, Addr)> {
-        let data = bs58::decode(&cursor)
+        let decoded = bs58::decode(cursor)
             .into_vec()
-            .map_err(|err| StdError::parse_err("Cursor", err.to_string()))?;
+            .map_err(|err| StdError::parse_err("Cursor", err))?;
 
-        let (hash_bytes, addr_str): (Vec<u8>, String) = serde_json::from_slice(&data)
-            .map_err(|err| StdError::parse_err("Cursor", err.to_string()))?;
+        let mut reader = BinaryReader::new(&decoded);
+        let hash = reader
+            .read_vec_u8()
+            .map_err(|err| StdError::parse_err("Cursor", err))?;
+        let addr = reader
+            .read_string()
+            .map_err(|err| StdError::parse_err("Cursor", err))?;
 
-        Ok((hash_bytes.into(), Addr::unchecked(addr_str)))
+        Ok((hash.into(), Addr::unchecked(addr)))
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn proper_encode() {
-        assert_eq!(encode(""), "".to_string());
-        assert_eq!(encode("an_id"), "BzZCCcK".to_string());
-    }
-
-    #[test]
-    fn proper_decode() {
-        assert_eq!(decode(""), Ok("".to_string()));
-        assert_eq!(decode("BzZCCcK"), Ok("an_id".to_string()));
-    }
-
-    #[test]
-    fn invalid_decode() {
-        assert_eq!(
-            decode("?"),
-            Err(StdError::parse_err(
-                "Cursor",
-                "provided string contained invalid character '?' at byte 0"
-            ))
-        );
-        assert_eq!(
-            decode("VtB5VXc"),
-            Err(StdError::parse_err(
-                "Cursor",
-                "invalid utf-8 sequence of 1 bytes from index 0"
-            ))
-        );
-    }
-}
+mod tests {}
